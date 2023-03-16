84026e97274614e9754f9e91738105ee20885ac1
Saishreekouda <saishreekouda@gmail.com>
2023-03-17 03:08:47 +0530

    Made Login page responsive

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Navbar from './components/Navbar';
+// import Signup from './components/Signup';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
 
-     <Signup/>
+     <Login/>
     </>
   )
 
