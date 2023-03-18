2fd7194f7cc80d2a32fdfe7c20f8663749ea43c8
Saishreekouda <saishreekouda@gmail.com>
2023-03-18 19:37:27 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Navbarhome from './components/Navbarhome';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+import Navbarhome from "./components/Navbarhome";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+// import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router";
+
 
 function App() {
   return (
-    
-
-    <BrowserRouter>
-    <Routes>
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        </Routes>
-  </BrowserRouter>
-  
- 
-    
+    <>
+    </>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/home" element={<Homepage />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/signup" element={<Signup />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
