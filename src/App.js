f5b2da384f39e6b79ed590961094dfc33397018f
Ananya <goyalananya444@gmail.com>
2023-03-18 19:32:45 +0530

    folder-structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Navbarhome from './components/Navbarhome';
 import Login from './pages/Login';
