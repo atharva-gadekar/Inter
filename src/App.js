a5b0d096aec2cc2393f21d44aa840d25b17851b4
Saishreekouda <saishreekouda@gmail.com>
2023-03-20 00:33:21 +0530

    components-made

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import Homepage from './pages/Homepage';
 import Navbarhome from './components/Navbarhome';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import CreatePost from './components/CreatePost';
+import ProfileSidebar from './components/ProfileSidebar';
+// import Post from './components/Post';
+// import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
+    
     <>
+      <CreatePost/>
     </>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/home" element={<Homepage />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/signup" element={<Signup />} />
-    //   </Routes>
-    // </BrowserRouter>
+  //   <BrowserRouter>
+  //   <Routes>
+  //       <Route path="/home" element={<Homepage />} />
+  //       <Route path="/login" element={<Login />} />
+  //       <Route path="/signup" element={<Signup />} />
+  //       </Routes>
+  // </BrowserRouter>
+  
+ 
+    
   );
 }
 
---
+++ src/components/CreatePost.js
@@ -0,0 +1,67 @@
+import React from "react";
+import postimg from "../assets/post.png";
+import profile from "../assets/profile.png";
+import {
+  faHome,
+  faUserFriends,
+  faBriefcase,
+  faEnvelope,
+  faBell,
+  faBuilding,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function CreatePost() {
+  return (
+      <>
+          <div className="p-12 w-1/3 bg-white ml-auto mr-auto shadow-md pb-16">
+              <img src={postimg} className=" mr-auto ml-auto outline outline-white -outline-offset-4"></img>
+      <h1 className="text-center font-bold pt-4 text-xl text-slate-800">
+        The meaning of health has evolved over time
+      </h1>
+      <div className="subheading flex justify-center items-center space-x-5 text-xs pt-2 mb-4 text-slate-500">
+        <p>July 26, 2019</p>
+        <div className="flex justify-center items-center space-x-2">
+          <p className="text-lg text-pink-900">-</p>
+          <p>1 min read</p>
+        </div>
+
+        <div className="flex justify-center items-center space-x-2">
+          {/* change building icon to comment icon */}
+          <FontAwesomeIcon icon={faBuilding} className="text-pink-700" />
+          <p>0</p>
+        </div>
+
+        <div className="flex justify-center items-center space-x-2">
+          {/* change building icon to fire icon */}
+          <FontAwesomeIcon icon={faBuilding} className="text-pink-700" />
+          <p>504</p>
+        </div>
+      </div>
+
+      <p className="text-left mr-auto ml-auto text-slate-500 text-base">
+        In keeping with the biomedical perspective, early definitions of health
+        focused on the theme of the body's ability to function; health was seen
+        as a state of normal function that could be disrupted from time to time
+        by disease. An..
+      </p>
+
+      <div className="footer mr-auto ml-auto flex justify-between items-center text-sm text-slate-800 mt-8">
+        <div className="flex justify-center items-left space-x-3">
+          <div className="tag bg-white shadow-md p-2 flex space-x-0"> <p className="text-yellow-600">#</p>Health</div>
+          <div className="tag bg-white shadow-md p-2 flex space-x-0"> <p className="text-purple-500">#</p>Travel</div>
+        </div>
+
+        <div className="flex justify-center items-center space-x-3">
+          <img src={profile} alt="" className="rounded-full h-8 w-8"></img>
+          <h3>Jonathan Doe</h3>
+        </div>
+      </div>
+          </div>
+          <div className=" mr-auto ml-auto text-center -mt-5">
+          <button className="bg-slate-700 text-white p-2 pl-4 pr-4 text-sm font-medium">Continue Reading</button>
+           </div>
+          
+      
+    </>
+  );
+}
