ff92a0366ef44a22aafb7211c786bb4153e46de9
Saishreekouda <saishreekouda@gmail.com>
2023-03-30 00:42:42 +0530

    blogpage responsive

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Homepage from './pages/Homepage';
 import Navbarhome from './components/Navbarhome';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+// import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import CreatePost from './components/CreatePost';
 import Chat from './components/Connect';
 import ProfileSidebar from './components/ProfileSidebar';
 import ImgUpload from './components/ImgUpload';
 import BlogPage from './components/BlogPage';
+import Post from './components/Post';
 
 
 
@@ -16,13 +17,14 @@ function App() {
   return (
 
 
-    <BrowserRouter>
-    <Routes>
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Signup />} />
-        </Routes>
-  </BrowserRouter>
+  //   <BrowserRouter>
+  //   <Routes>
+  //       <Route path="/home" element={<Homepage />} />
+  //       <Route path="/login" element={<Login />} />
+  //       <Route path="/" element={<Signup />} />
+  //       </Routes>
+  // </BrowserRouter>
+    <BlogPage></BlogPage>
   
  
     
