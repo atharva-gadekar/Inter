95e1e879be9e24bc2b1b974b80623ae7637a3f62
Ananya <goyalananya444@gmail.com>
2023-04-10 14:06:34 +0530

    modified

--- src/components/Navbarhome.js
+++ src/components/Navbarhome.js
@@ -8,7 +8,7 @@ const Navbarhome = () => {
   const [ismobileopen, setmobileopen] = useState(false);
   const [userName, setUserName] = useState('');
   const token = localStorage.getItem('token');
-
+const[url,setUrl]=useState('');
   useEffect(() => {
     const fetchUserName = async () => {
       try {
@@ -27,6 +27,7 @@ const Navbarhome = () => {
           const data = await response.json();
           console.log(data);
           setUserName(data.user.name);
+          setUrl(data.url);
         }
       } 
       
@@ -48,7 +49,7 @@ const Navbarhome = () => {
       <nav className="lg:flex hidden items-center justify-between flex-wrap bg-white p-4 rounded-2xl my-4 mx-5">
         <div className="flex items-center flex-shrink-0 text-gray-800">
           <div className="flex items-center">
-            <img src={logo} alt="Logo" className="h-8 w-8 mr-2 rounded-md" />
+            <img src={url} alt="Logo" className="h-8 w-8 mr-2 rounded-md" />
             <h1 className="font-semibold text-base tracking-tight">{userName}</h1>
           </div>
         </div>
