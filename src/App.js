43454255345c3d02c8c02f3215c1d3c5f476a226
Ananya <goyalananya444@gmail.com>
2023-04-14 20:36:29 +0530

    forgot-pass

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import React from "react";
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import ConnectionsPage from './pages/ConnectionsPage';
-import ChatFinal from './pages/ChatFinal';
-import Blog from './components/Blog';
-import ProfileFinal from './pages/ProfileFinal';
-import Notifications from './pages/Notifications';
-
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ConnectionsPage from "./pages/ConnectionsPage";
+import ChatFinal from "./pages/ChatFinal";
+import Blog from "./components/Blog";
+import ProfileFinal from "./pages/ProfileFinal";
+import Notifications from "./pages/Notifications";
+import Forgotpass from "./pages/Forgotpass";
 function App() {
-  
   return (
-
-    
     <BrowserRouter>
-    <Routes>
+      <Routes>
         <Route path="/home" element={<Homepage />} />
-        <Route path="/login" element={<Login />}/> 
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/" element={<Login />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Login />} />
         <Route path="/profile/:id" element={<ProfileFinal />} />
         <Route path="/blog/:id" element={<Blog />} />
         <Route path="/chat" element={<ChatFinal />} />
         <Route path="/network" element={<ConnectionsPage />} />
         <Route path="/notifications" element={<Notifications />} />
-        </Routes>
-  </BrowserRouter>
-    
-   
-    
-
-   
-  
- 
-    
+        <Route path="forgot" element={<Forgotpass />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Forgotpass.js
+++ src/pages/Forgotpass.js
@@ -9,50 +9,50 @@ import { Spin } from "antd";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-function Login() {
+function Forgot() {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const token = localStorage.getItem("token");
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+
     const data = {
       email: email,
-      password: password,
     };
 
     try {
-      const response = await axios.post(
-        "https://inter-api-8q0x.onrender.com/auth/login",
-        data
+      const response = await axios.put(
+        `https://inter-api-8q0x.onrender.com/auth/forgot-password`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
+
       console.log(response);
-      const token = response.data.jwt_token;
-      const userId = response.data.id;
-      if (token) {
-        localStorage.setItem("token", token);
-        localStorage.setItem("userId", userId);
-        message.success("Login successful!");
-        navigate("/home");
-        setLoading(false);
-      }
-    } catch (error) {
+      message.success("Email sent successfully!");
+      setLoading(false);
+    } 
+
+    catch (error) {
       setLoading(false);
       console.error(error);
-      message.error("Login failed. Please check your email and password!");
+      message.error("Failed to send email. Please try again!");
     }
   };
 
   return (
     <div className="bg-gradient-to-br from-blue-100 to-white-400">
-      <Navbar />
+      <Navbar className="mt-3" />
       <div className="block lg:flex items-center justify-center min-h-[90vh] overflow-y-hidden">
         <div className="block lg:flex flex-col mt-20 ">
           {/* headings and subheadings */}
@@ -88,50 +88,15 @@ function Login() {
                 className="w-full px-4 py-3 bg-[#dfe6f9] border-gray-300 rounded-lg"
               />
             </div>
-            <div className="flex flex-col relative">
-  <input
-    type={showPassword ? "text" : "password"}
-    id="password"
-    name="password"
-    onChange={(e) => setPassword(e.target.value)}
-    placeholder="Enter password"
-    className="w-full px-4 py-3 bg-[#dfe6f9] border-gray-300 rounded-lg"
-  />
-  <button
-  type="button"
-    className="absolute top-1/2 transform -translate-y-1/2 right-7"
-    onClick={handleTogglePassword}
-  >
-    {showPassword ? (
-      <FaEyeSlash className="text-gray-600" />
-    ) : (
-      <FaEye className="text-gray-600" />
-    )}
-  </button>
-</div>
 
-
-            <div className="flex justify-between items-center">
-              <div className="items-center hidden lg:flex">
-                <input type="checkbox" className="mr-2" />
-                <label>Keep me logged in</label>
-              </div>
-              <Link to="/forgot">
-              <a href="#" className="text-blue-500 font-medium">
-                Forgot password?
-              </a>
-              </Link>
-            </div>
-            {/* <Link to='/home'> */}
             <button
               type="submit"
               className="w-full mt-1 lg:mt-3 px-4 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-lg"
             >
-              SIGN IN
+              SEND EMAIL
             </button>
             {!loading && <Spin className="opacity-0" />}
             {loading && <Spin />}
-            {/* </Link> */}
           </form>
         </div>
       </div>
@@ -139,4 +104,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Forgot;
