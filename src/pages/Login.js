069f34380f0a855d7b08ec9f8f9844292fe181dc
Ananya <goyalananya444@gmail.com>
2023-04-15 19:04:41 +0530

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,6 +9,8 @@ import { Spin } from "antd";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useEffect } from "react";
+
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -20,9 +22,18 @@ function Login() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+
     const data = {
       email: email,
       password: password,
@@ -39,17 +50,18 @@ function Login() {
       if (token) {
         localStorage.setItem("token", token);
         localStorage.setItem("userId", userId);
+        
+
         message.success("Login successful!");
         navigate("/home");
-        setLoading(false);
       }
     } catch (error) {
-      setLoading(false);
       console.error(error);
       message.error("Login failed. Please check your email and password!");
     }
   };
 
+ 
   return (
     <div className="bg-gradient-to-br from-blue-100 to-white-400">
       <Navbar />
@@ -89,37 +101,40 @@ function Login() {
               />
             </div>
             <div className="flex flex-col relative">
-  <input
-    type={showPassword ? "text" : "password"}
-    id="password"
-    name="password"
-    onChange={(e) => setPassword(e.target.value)}
-    placeholder="Enter password"
-    className="w-full px-4 py-3 bg-[#dfe6f9] border-gray-300 rounded-lg"
-  />
-  <button
-  type="button"
-    className="absolute top-1/2 transform -translate-y-1/2 right-7"
-    onClick={handleTogglePassword}
-  >
-    {showPassword ? (
-      <FaEyeSlash className="text-gray-600" />
-    ) : (
-      <FaEye className="text-gray-600" />
-    )}
-  </button>
-</div>
-
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter password"
+                className="w-full px-4 py-3 bg-[#dfe6f9] border-gray-300 rounded-lg"
+              />
+              <button
+                type="button"
+                className="absolute top-1/2 transform -translate-y-1/2 right-7"
+                onClick={handleTogglePassword}
+              >
+                {showPassword ? (
+                  <FaEyeSlash className="text-gray-600" />
+                ) : (
+                  <FaEye className="text-gray-600" />
+                )}
+              </button>
+            </div>
 
             <div className="flex justify-between items-center">
               <div className="items-center hidden lg:flex">
-                <input type="checkbox" className="mr-2" />
+                <input
+                  type="checkbox"
+                  className="mr-2"
+                 
+                />
                 <label>Keep me logged in</label>
               </div>
               <Link to="/forgot">
-              <a href="#" className="text-blue-500 font-medium">
-                Forgot password?
-              </a>
+                <a href="#" className="text-blue-500 font-medium">
+                  Forgot password?
+                </a>
               </Link>
             </div>
             {/* <Link to='/home'> */}
