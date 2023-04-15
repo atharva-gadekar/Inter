03908eeafbc1b4aed09a88976c889bcf188f6fe3
atharva-gadekar <atharvagadekar@gmail.com>
2023-04-15 22:16:57 +0530

    Network Page Partially Done

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -10,28 +10,110 @@ import '../App.css';
 import { Link } from "react-router-dom";
 import NavigationHome from './NavigationHome';
 import UserProfile from './UserProfile';
-const Connections = () => {
- 
-  return (
-    <div className='bg-white p-8 rounded-3xl'>
-      <h1 className='my-4 font-bold text-3xl text-blue-600'>Interests</h1>
-      <div className="flex justify-left space-x-3 items-center  mb-12">
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200">All</button>
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200">Design</button>
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200">Marketing</button>
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200 ">Web Development</button>
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200 ">ML</button>
-        <button className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200">App Development</button>
+import { useEffect } from 'react';
+import axios from 'axios';
 
-      </div>
-      <div className="flex justify-evenly space-x-12  mb-5">
-        <UserProfile />
-        <UserProfile />
-        <UserProfile/>
-      </div>
-    </div>
-    
-  );
+
+const Connections = () => {
+    const token = localStorage.getItem("token");
+	const userId = localStorage.getItem("userId");
+	const [userInterests, setUserInterests] = React.useState(null);
+	const [connections, setConnections] = React.useState(null);
+
+	const getConnections = (interest) => {
+		axios({
+			method: "get",
+			url: `https://inter-api-8q0x.onrender.com/user/${userId}/${interest}`,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).then((response) => {
+			console.log(response.data);
+			setConnections(response.data);
+		});
+	}
+
+	const getConnectionsAll = () => {
+		axios({
+			method: "get",
+			url: `https://inter-api-8q0x.onrender.com/user/${userId}/interests/all`,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).then((response) => {
+			console.log(response.data);
+			setConnections(response.data);
+		});
+	}
+
+
+   useEffect(() => {
+			const fetchUserName = async () => {
+				try {
+					if (token) {
+						axios({
+							method: "get",
+							url: `https://inter-api-8q0x.onrender.com/user/${userId}`,
+							headers: {
+								Authorization: `Bearer ${token}`,
+							},
+						}).then((response) => {
+							console.log(response.data);
+							setUserInterests(response.data.user.interests);
+						});
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			};
+			fetchUserName();
+
+				const getConnectionsAll = () => {
+					axios({
+						method: "get",
+						url: `https://inter-api-8q0x.onrender.com/user/${userId}/interests/all`,
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}).then((response) => {
+						console.log(response.data);
+						setConnections(response.data);
+					});
+				};
+				getConnectionsAll();
+
+		}, []);
+
+  return (
+		<div className="bg-white p-8 rounded-3xl">
+			<h1 className="my-4 font-bold text-3xl text-blue-600">Interests</h1>
+			<div className="flex justify-left space-x-3 items-center  mb-12">
+				<button
+					className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200"
+					onClick={() => {
+						getConnectionsAll();
+					}}
+				>
+					All
+				</button>
+				{userInterests &&
+					userInterests.map((interest) => (
+						<button
+							className="px-5 py-2 border border-blue-100 rounded-3xl text-blue-700 bg-blue-100 font-medium focus:bg-blue-200"
+							onClick={() => {
+								getConnections(interest);
+							}}
+						>
+							{interest}
+						</button>
+					))}
+			</div>
+			<div className="flex justify-evenly space-x-12  mb-5">
+				{connections &&
+					connections.map((connection) => <UserProfile user={connection} />)}
+			</div>
+		</div>
+	);
 };
 
 export default Connections;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,105 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-faAdd,
-faCamera,
-faFileAlt,
-faHome,
-faInbox,
-faLink,
-faMessage,
-faPerson,
-faSearch,
-faUser,
+	faAdd,
+	faCamera,
+	faFileAlt,
+	faHome,
+	faInbox,
+	faLink,
+	faMessage,
+	faPerson,
+	faSearch,
+	faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Modal, Input } from "antd"; 
+import { Modal, Input } from "antd";
 
 import profile_bg from "../assets/Twitterspace_Aptod.png";
 import profile from "../assets/Rectangle 47.png";
+import userEvent from "@testing-library/user-event";
 
-export default function UserProfile() {
-const [isModalVisible, setIsModalVisible] = useState(false); 
-const [messageTitle, setMessageTitle] = useState(""); 
-const [messageContent, setMessageContent] = useState(""); 
+export default function UserProfile({user}) {
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [messageTitle, setMessageTitle] = useState("");
+	const [messageContent, setMessageContent] = useState("");
 
-const handleOk = () => { 
+	const handleOk = () => {
+		setIsModalVisible(false);
+	};
 
-setIsModalVisible(false);
-};
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
 
-const handleCancel = () => { 
-setIsModalVisible(false);
-};
+  useEffect(()=>{
+    console.log(user);
+  },[])
 
-return (
-<div>
-<div className=" bg-white h-full rounded-3xl shadow-lg">
-<img
-       src={profile_bg}
-       alt=""
-       className="rounded-3xl mt-4 overflow-hidden object-cover w-full"
-     />
-<img
-       src={profile}
-       className="-mt-12 mr-auto ml-auto rounded-2xl h-20 w-20"
-     ></img>
-<h1 className="text-center font-bold text-2xl mt-4 mb-6">Nick Rybak</h1>
-<p className="text-center text-slate-500 mt-3 text-lg px-6">
-UX/UI designer- Phoenix Agency
-</p>
-<p className="text-center text-slate-500 mt-3 text-base px-6">
-      Indian Institute of Technology Delhi
-    </p>
+	return (
+		<div>
+			<div className=" bg-white h-full rounded-3xl shadow-lg">
+				<img
+					src={profile_bg}
+					alt=""
+					className="rounded-3xl mt-4 overflow-hidden object-cover w-full"
+				/>
+        {user.url && (
+          <img
+					src={user.url}
+					className="-mt-12 mr-auto ml-auto rounded-2xl h-20 w-20"
+				/>) || (<img
+					src={profile}
+					className="-mt-12 mr-auto ml-auto rounded-2xl h-20 w-20"
+				/>)
+        }
+				
+				<h1 className="text-center font-bold text-2xl mt-4 mb-6">{user.user.name}</h1>
+				<p className="text-center text-slate-500 mt-3 text-lg px-6">
+        {user.user.title}
+				</p>
+				<p className="text-center text-slate-500 mt-3 text-base px-6">
+          {user.user.collegeName}
+				</p>
 
-    <p className="text-center text-slate-500 mt-1 mb-6 text-base px-6 ">
-      Computer Science
-    </p>
+				<p className="text-center text-slate-500 mt-1 mb-6 text-base px-6 ">
+          {user.user.branch}
+				</p>
 
-    <div className="flex justify-center space-x-3 mb-6">
-      <button className="bg-blue-100 py-2 px-3 text-blue-600 font-medium rounded-3xl text-sm">Design</button>
-      <button className="bg-blue-100 py-2 px-3 text-blue-600 font-medium rounded-3xl text-sm">Cloud</button>
-      <button className="bg-blue-100 py-2 px-3 text-blue-600 font-medium rounded-3xl text-sm">AI</button>
-    </div>
 
-    <div className=" flex justify-center items-center space-x-3">
-      <button
-        className="px-4 py-2 text-blue-600 font-normal "
-        onClick={() => setIsModalVisible(true)} // Set modal visibility to true on "Connect" button click
-      >
-        Connect
-      </button>
-      <button className="px-4 py-2 text-slate-500 font-normal">
-        Ignore
-      </button>
-    </div>
 
-   
-    <Modal
-    className="mt-10 "
-      title="Send a message"
-      visible={isModalVisible}
-      onOk={handleOk}
-      onCancel={handleCancel}
-    >
-      <Input
-        placeholder="Title"
-        value={messageTitle}
-        className="mb-3"
-        onChange={(e) => setMessageTitle(e.target.value)}
-      />
-      
-           <Input.TextArea
-           
-             placeholder="Enter message content"
-             value={messageContent} 
-             onChange={(e) => setMessageContent(e.target.value)} 
-             allowClear
-           />
-         
-         </Modal>
-       </div>
-       
-       </div>
-)
+				<div className="flex justify-center space-x-3 mb-6">
+          {user.user && user.user.interests.map((interest) => (
+            <button className="bg-blue-100 py-2 px-3 text-blue-600 font-medium rounded-3xl text-sm">
+                {interest}
+              </button>
+          )
+          )}
+				</div>
 
-};
+				<div className=" flex justify-center items-center space-x-3">
+					<button
+						className="px-4 py-2 text-blue-600 font-normal "
+						onClick={() => setIsModalVisible(true)} // Set modal visibility to true on "Connect" button click
+					>
+						Connect
+					</button>
+					<button className="px-4 py-2 text-slate-500 font-normal">
+						Ignore
+					</button>
+				</div>
+
+				<Modal
+					className="mt-10 "
+					title="Send a message"
+					visible={isModalVisible}
+					onOk={handleOk}
+					onCancel={handleCancel}
+				>
+					<Input.TextArea
+						placeholder="Send a Connection Request Message"
+						value={messageContent}
+						onChange={(e) => setMessageContent(e.target.value)}
+						allowClear
+					/>
+				</Modal>
+			</div>
+		</div>
+	);
+}
