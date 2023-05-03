ae809caa0fa43e820ac37608ab725d755e794c04
Ananya <goyalananya444@gmail.com>
2023-05-03 14:51:05 +0530

    done

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,33 +1,66 @@
-import { Modal, Checkbox } from 'antd';
-import { useState } from 'react';
+import { useState } from "react";
+import { Modal, Button } from "antd";
 
-const ModalComponent = ({ visible, onOk, onCancel }) => {
-  const [tags, setTags] = useState([]);
+const ModalComponent = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const handleTagChange = (tag, checked) => {
-    const newTags = checked
-      ? [...tags, tag]
-      : tags.filter((t) => t !== tag);
-    setTags(newTags);
+  const showModal = () => {
+    setIsModalVisible(true);
   };
 
   const handleOk = () => {
-    onOk(tags);
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
   };
 
   return (
-    <Modal
-      title="Select your tags"
-      visible={visible}
-      onOk={handleOk}
-      onCancel={onCancel}
+    <>
+      <Button type="primary" onClick={showModal}>
+        Click Me
+      </Button>
+      <Modal
+        title="Select Your Tags"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Submit
+          </Button>,
+        ]}
+      >
+        <div className="flex flex-wrap">
+          <Tag />
+          <Tag />
+          <Tag />
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const Tag = () => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleSelect = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <div
+      className={`px-3 py-2 rounded-md cursor-pointer m-1 ${
+        isSelected ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"
+      }`}
+      onClick={handleSelect}
     >
-      <Checkbox.Group onChange={handleTagChange} value={tags}>
-        <Checkbox value="webd">Web Development</Checkbox>
-        <Checkbox value="appd">App Development</Checkbox>
-        <Checkbox value="aiml">AI/ML</Checkbox>
-      </Checkbox.Group>
-    </Modal>
+      {isSelected ? "Selected" : "Not Selected"}
+    </div>
   );
 };
 
