82f3313d296bb84dbd100a88a3394655ba8beb61
Saishreekouda <saishreekouda@gmail.com>
2023-05-04 15:30:14 +0530

    changes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect, useState, useRef } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 // import post from "../assets/post.png"
 import post from "../assets/post.png";
@@ -21,16 +21,15 @@ import moment from "moment";
 import { UserContext } from "../utils/context/UserContext";
 
 export default function Blog() {
-	const { user, setUser, token } = useContext(UserContext);
- 
+  const { user, setUser, token } = useContext(UserContext);
 
   const userId = localStorage.getItem("userId");
   let blogID = useParams().id;
   const [blog, setBlog] = useState({});
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
-  
-	const [loading, setLoading] = useState(true);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBlog = async () => {
@@ -46,7 +45,6 @@ export default function Blog() {
           setLoading(false);
           setBlog(response.data);
           setComments(response.data.blog.comments);
-          
         });
       } catch (error) {
         console.error(error);
@@ -101,128 +99,130 @@ export default function Blog() {
       });
   };
 
- 
   const postComment = async () => {
-	try {
-	  const response = await axios.post(
-		`https://inter-api-8q0x.onrender.com/blog/${blogID}/comment`,
-		{
-		  content: newComment,
-		  author: userId,
-		  blogID: blogID
-		},
-		{
-		  headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${token}`
-		  }
-		}
-	  );
-	  console.log(response.data.comment); 
-	  
-	  setComments([...comments, response.data.comment]);
-	  setNewComment('');
-	} catch (error) {
-	  console.error(error);
-	}
+    try {
+      const response = await axios.post(
+        `https://inter-api-8q0x.onrender.com/blog/${blogID}/comment`,
+        {
+          content: newComment,
+          author: userId,
+          blogID: blogID,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data.comment);
+
+      setComments([...comments, response.data.comment]);
+      setNewComment("");
+    } catch (error) {
+      console.error(error);
+    }
   };
-  
 
-
-
-  
   return (
-		<>
-			<Navbarhome />
-      {loading && <ArticleLoader/>}
-			{blog.blog && (
-				<div className=" lg:w-[70%] mr-auto ml-auto mt-12">
-					<img
-						src={blog.url}
-						className=" ml-auto mr-auto w-[95%] max-h-96 object-contain"
-					></img>
-					<div className="flex items-center space-x-4 mt-4 lg:mt-12 lg:mb-6 mb-4 px-24">
-						{user.url && (
-							<img src={user.url} alt="" className="h-8 w-8 rounded-full" />
-						)}
-						<p className="text-xs lg:text-sm text-slate-600">
-							{user.user && (
-								<Link to={`/user/${user.user._id}`}>{user.user.name}</Link>
-							)}
-						</p>
-					</div>
-					<div className="lg:flex space-x-12 lg:place-items-start ">
-						<div className="flex lg:flex-col lg:space-y-3 pb-4 lg:pt-2 lg:pb-2 justify-center">
-							<FontAwesomeIcon
-								icon={faShareNodes}
-								className="text-4xl font-thin"
-								onClick={handleCopyClick}
-							/>
-						</div>
-						<div className="justify-center ml-auto mr-auto">
-							<h2 className="font-extrabold text-2xl lg:text-5xl text-slate-800 lg:leading-[1.25] mr-12 lg:mr-auto">
-								{blog.blog.title}
-							</h2>
-							<p className="text-slate-600 mt-4 mr-12 lg:mr-auto mb-10">
-								{blog.blog.brief}
-							</p>
+    <>
+      <Navbarhome />
+      {loading && <ArticleLoader />}
+      {blog.blog && (
+        <div className=" lg:w-[70%] mr-auto ml-auto mt-12">
+          <img
+            src={blog.url}
+            className=" ml-auto mr-auto w-[95%] max-h-96 object-contain"
+          ></img>
+          <div className="flex items-center space-x-4 mt-4 lg:mt-12 lg:mb-6 mb-4 px-24">
+            {user.url && (
+              <img src={user.url} alt="" className="h-8 w-8 rounded-full" />
+            )}
+            <p className="text-xs lg:text-sm text-slate-600">
+              {user.user && (
+                <Link to={`/user/${user.user._id}`}>{user.user.name}</Link>
+              )}
+            </p>
+          </div>
+          <div className="lg:flex space-x-12 lg:place-items-start ">
+            <div className="flex lg:flex-col lg:space-y-3 pb-4 lg:pt-2 lg:pb-2 justify-center">
+              <FontAwesomeIcon
+                icon={faShareNodes}
+                className="text-4xl font-thin"
+                onClick={handleCopyClick}
+              />
+            </div>
+            <div className="justify-center ml-auto mr-auto">
+              <h2 className="font-extrabold text-2xl lg:text-5xl text-slate-800 lg:leading-[1.25] mr-12 lg:mr-auto">
+                {blog.blog.title}
+              </h2>
+              <p className="text-slate-600 mt-4 mr-12 lg:mr-auto mb-10">
+                {blog.blog.brief}
+              </p>
 
-							<div
-								className="content mb-10"
-								dangerouslySetInnerHTML={{ __html: blog.blog.content }}
-							></div>
+              <div
+                className="content mb-10"
+                dangerouslySetInnerHTML={{ __html: blog.blog.content }}
+              ></div>
 
-							{/* comment section */}
+              {/* comment section */}
 
-							<hr className="mr-12 lg:mr-auto"></hr>
-							<div className="flex p-1 pt-5 space-x-12 mb-20">
-  <img src={user.user.url} alt="" className="rounded-full h-8 w-8" />
-  <div className="flex-wrap ">
-  <input
-      type="text"
-      placeholder="Write a comment..."
-      value={newComment}
-      onChange={(event) => setNewComment(event.target.value)}
-      className="text-slate-400 text-sm !outline-none bg-transparent w-[200%]"
-    />
-    <div className="flex mt-10 ">
-	<button className='bg-blue-600 text-white text-sm px-4 py-2 rounded-md font-medium' onClick={postComment}>
-                            Post Comment
-                        </button>
-    </div>
-  </div>
-</div>
+              <hr className="w-[100%]"></hr>
+              <div className="flex p-1 pt-5 space-x-12 mb-20">
+                <img
+                  src={user.user.url}
+                  alt=""
+                  className="rounded-full h-8 w-8"
+                />
+                <div className="flex-wrap ">
+                  <input
+                    type="text"
+                    placeholder="Write a comment..."
+                    value={newComment}
+                    onChange={(event) => setNewComment(event.target.value)}
+                    className="text-slate-400 text-sm !outline-none bg-transparent w-[200%]"
+                  />
+                  <div className="flex mt-10 ">
+                    <button
+                      className="bg-blue-600 text-white text-sm px-4 py-2 rounded-md font-medium"
+                      onClick={postComment}
+                    >
+                      Post Comment
+                    </button>
+                  </div>
+                </div>
+              </div>
 
-							<hr className="mr-12 lg:mr-auto"></hr>
+              <hr className=" lg:mr-auto"></hr>
 
-							{comments.length !== 0 &&
-								comments.map((comment) => (
-									<div
-										key={comment.blogID}
-										className="flex p-1  pt-5 space-x-12 mr-12 lg:mr-auto"
-									>
-										<img
-											src={comment.author.url}
-											alt=""
-											className="rounded-full h-8 w-8"
-										/>
-										<div>
-											<h1 className="text-slate-800 font-bold text-sm">
-												{comment.author.name}
-											</h1>
-											<p className="text-slate-400 text-xs -mt-0 mb-6">
-												{moment(comment.createdAt).fromNow()}
-											</p>
-											<p className="text-slate-400 text-sm mb-14">
-												{comment.content}
-											</p>
-										</div>
-									</div>
-								))}
-						</div>
-					</div>
-				</div>
-			)}
-		</>
-	);
+              {comments.length !== 0 &&
+                comments.map((comment) => (
+                  <div
+                    key={comment.blogID}
+                    className="flex p-1  pt-5 space-x-12 mr-12 lg:mr-auto"
+                  >
+                    <img
+                      src={comment.author.url}
+                      alt=""
+                      className="rounded-full h-8 w-8"
+                    />
+                    <div>
+                      <h1 className="text-slate-800 font-bold text-sm">
+                        {comment.author.name}
+                      </h1>
+                      <p className="text-slate-400 text-xs -mt-0 mb-6">
+                        {moment(comment.createdAt).fromNow()}
+                      </p>
+                      <p className="text-slate-400 text-sm mb-14">
+                        {comment.content}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
